ceb3f67e76fc7acfcdbf5b77a9d6074dd0611c05
Aldair <aldaprojects@gmail.com>
2020-10-11 18:56:36 -0500

    docs added and change env variables

---
+++ docs/precache-manifest.4399f6e386b5ec2d591d5d6732ee5caa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e91e1d4537623a77033dd8582ab4e40e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "437f1859f32661bf50ff",
+    "url": "/static/css/main.d79e415e.chunk.css"
+  },
+  {
+    "revision": "2869b83fab855abd5633",
+    "url": "/static/js/2.6da5ec11.chunk.js"
+  },
+  {
+    "revision": "3be25bf51018a491ef7f73dfd2dd74c4",
+    "url": "/static/js/2.6da5ec11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "437f1859f32661bf50ff",
+    "url": "/static/js/main.ef2d27f2.chunk.js"
+  },
+  {
+    "revision": "c17e559ba43e3d40c625",
+    "url": "/static/js/runtime-main.cfe225b0.js"
+  }
+]);
--- src/env/env.js
+++ src/env/env.js
@@ -1,1 +1,2 @@
-export const URL = 'http://localhost:8000'
+// export const URL = 'http://localhost:8000'
+export const URL = 'https://chat-uat-api.herokuapp.com';
