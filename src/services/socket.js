bed914dae1fd3a00cf6b0e7d30a02d10159429b7
Aldair <aldaprojects@gmail.com>
2020-10-11 19:04:45 -0500

    update url

--- src/services/socket.js
+++ src/services/socket.js
@@ -1,6 +1,8 @@
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:8000', {
+import { URL } from '../env/env';
+
+const socket = io(URL, {
     transports: ['websocket'], 
     upgrade: false
 });
