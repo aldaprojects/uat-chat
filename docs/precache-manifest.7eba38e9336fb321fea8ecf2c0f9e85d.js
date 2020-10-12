394f1e783112a8eb4115026df618a1aecc21d788
Aldair <aldaprojects@gmail.com>
2020-10-11 19:25:43 -0500

    updated html

---
+++ docs/precache-manifest.7eba38e9336fb321fea8ecf2c0f9e85d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7344160e49bb054b79796bc96ae2b06d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eaeb58ad862c7065539e",
+    "url": "/static/css/main.d79e415e.chunk.css"
+  },
+  {
+    "revision": "2869b83fab855abd5633",
+    "url": "/static/js/2.6da5ec11.chunk.js"
+  },
+  {
+    "revision": "3be25bf51018a491ef7f73dfd2dd74c4",
+    "url": "/static/js/2.6da5ec11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eaeb58ad862c7065539e",
+    "url": "/static/js/main.77f99443.chunk.js"
+  },
+  {
+    "revision": "c17e559ba43e3d40c625",
+    "url": "/static/js/runtime-main.cfe225b0.js"
+  }
+]);
